bed8c7dfae4b252b3bf5f45b7f64454f0772f6b8
Refaim Daza <refaimdazacalixto@gmail.com>
2022-11-15 22:05:34 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// Ejercicio 01
+// let a = +prompt('ingrese el numero de varones')
+// let b = +prompt('ingrese el numero de mujeres')
+// let c = a +b 
+// alert(`El porcentaje de hombres es ${a*100/c}%`)
+// alert(`El porcentaje de mujeres es ${b*100/c}%`)
+
+// Ejercicio 02
+// let a = +prompt('Ingrese el monto de inversion en soles')
+// alert(`En la avenida La mar se invertira ${a*35/100} soles`)
+// alert(`En la avenida Abancay se invertira ${a*25/100} soles`)
+// alert(`En la avenida 28 de Julio se invertira ${a*10/100} soles`)
+// alert(`En la avenida Aviacion se invertira ${a*15/100} soles`)
+// alert(`En la avenida Tacna se invertira ${a*15/100} soles`)
+
+// Ejercicio 03
+// let a = +prompt('Ingrese la cantidad de entradas generales vendidas')
+// let b = +prompt('Ingrese la cantidad de entradas para mayores de 65 años vendidas')
+// let c = +prompt('Ingrese la cantidad de entradas para menores de edad vendidas')
+// let totalA = a*150
+// let totalB = b*50
+// let totalC = c*80
+// alert(`Se vendiieron ${totalA}`)
+// alert(`Se vendieron ${totalB}`)
+// alert(`Se vendieron ${totalC}`)
+// alert(`El total vendido es ${totalA + totalB + totalC}`)
