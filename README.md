19c722b7b8ab26e904d2009bd3aaf25028ead858
Refaim Daza <refaimdazacalixto@gmail.com>
2022-11-17 20:43:16 -0500

    subiendo script de la sesion 16

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Hola Mundo
 Subiendo script de la sesion 15
+Subiendo script de la sesion 16
---
+++ s16.js
@@ -0,0 +1,43 @@
+// const multi = () => {
+//   return x * y;
+// };
+
+// const expon = () => {
+//   return Math.pow(x, y);
+// };
+
+// let suma = (x, y) => {
+//   return x + y;
+// };
+
+// let result = multi(5, 2);
+// alert(result);
+
+// result = suma(3, 4);
+// alert(result);
+
+
+// Ejercicio 01
+// const a = +prompt('Ingrese el primer numero')
+// const b = +prompt('ingrese el segundo numero')
+// const restar =(a, b) =>{
+//     return a-b
+// }
+// alert(`La resta es ${restar(a, b)}`)
+
+// Ejercicio 02
+// let grado = +prompt('Ingrese el valor en Farenheit')
+// const convertidor = (a)=> {
+//     return (a - 32) * 5/9
+// }
+// alert(`${convertidor(grado)} Grados celsius`)
+
+// Ejercicio 03
+// let a =+prompt('Ingrese el primer numero')
+// let b =+prompt('Ingrese el segundo numero')
+
+
+// const sumar = (a, b)=> {
+//     return a+b
+// }
+// document.write(`La suma es ${sumar(a,b)}`)
